bc547e95576ab4c05d84fb0b109b3cc89fcca9a5
tadicbb <tadicbb@gmail.com>
2022-01-19 21:23:29 +0100

    cotroller update name

---
+++ OpenExchangeRates.Api/Controllers/CurrencyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenExchangeRates.Api.BL.Interfaces;
+
+namespace OpenExchangeRates.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        
+        private readonly HttpClient _httpClient;
+        private readonly ICurrencyService _service;
+        public CurrencyController(
+                                        HttpClient httpClient,
+                                        ICurrencyService service)
+        {
+            
+            _httpClient = httpClient;
+            _service = service;
+        }
+        [HttpGet]
+        [Route("Result")]
+        public IActionResult Result()
+        {
+            var result = _service.GetAvg();
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+    }
+}
